10581dd0aa88b9dc1de7c8111103607bd4791076
maseerabaig <maseerabaig13@gmail.com>
2025-05-24 13:23:40 +0530

    Initial commit - email sending app using Node.js and Nodemailer

---
+++ README.md
@@ -0,0 +1,8 @@
+# SendMail App
+
+## Setup Instructions
+
+1. Clone the repo or download the folder
+2. Run `npm install`
+3. Create a `.env` file like this:
+
---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.get("/sendemail", async (req, res) => {
+  try {
+   
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    
+    const mailOptions = {
+      from: `"Masai NEM Student" <${process.env.EMAIL}>`,
+      to: `db7583777@gmail.com, venugopal.burli@masaischool.com`,
+      subject: "Test Email from NEM Student",
+      text: "This is a testing Mail sent by NEM student, no need to reply.",
+    };
+
+    
+    await transporter.sendMail(mailOptions);
+
+    res.send("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Error sending email.");
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
